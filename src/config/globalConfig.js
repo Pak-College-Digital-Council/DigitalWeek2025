6ac3b41439054222a5e145c9173b2806ec897dd0
Cryptic1526 <119151348+Cryptic1526@users.noreply.github.com>
2025-08-13 08:42:08 +1200

    Update globalConfig.js

--- src/config/globalConfig.js
+++ src/config/globalConfig.js
@@ -2,7 +2,7 @@
 // Day 2: File Sorting Challenge (File Explorer)
 // Day 3: EMails (Email Client)
 // Day 3.5: Finale Terminal (after Day 3 completion)
-export const ACTIVE_DAY = 2;
+export const ACTIVE_DAY = 3;
 
 // skip to finale terminal for testing
 export const DEV_SKIP_TO_FINALE = false;
